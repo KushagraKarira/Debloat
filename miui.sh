60f65b9f394ef0cf717a109534a80c039c814165
KushagraKarira <36698785+KushagraKarira@users.noreply.github.com>
2019-03-06 22:54:29 +0530

    Sorted and Duplicates removed

--- miui.sh
+++ miui.sh
@@ -1,42 +1,63 @@
 #!/bin/sh
-adb shell pm uninstall --user 0 com.android.calendar
-adb shell pm uninstall --user 0 com.android.chrome 
-adb shell pm uninstall --user 0 com.android.mms
+adb shell pm uninstall --user 0 com.android.browser #Mi Browser
+adb shell pm uninstall --user 0 com.android.calendar #Calendar
+adb shell pm uninstall --user 0 com.android.chrome #Google Chrome
+adb shell pm uninstall --user 0 com.android.mms #Messages
 adb shell pm uninstall --user 0 com.autonavi.minimap
 adb shell pm uninstall --user 0 com.baidu.duersdk.opensdk
 adb shell pm uninstall --user 0 com.baidu.input_mi
 adb shell pm uninstall --user 0 com.baidu.searchbox
 adb shell pm uninstall --user 0 com.caf.fmradio
 adb shell pm uninstall --user 0 com.duokan.phone.remotecontroller
+adb shell pm uninstall --user 0 com.facebook.appmanager #Facebook App Manager
 adb shell pm uninstall --user 0 com.facebook.katana
-adb shell pm uninstall --user 0 com.google.android.apps.docs
-adb shell pm uninstall --user 0 com.google.android.apps.maps
+adb shell pm uninstall --user 0 com.facebook.services #Facebook Services
+adb shell pm uninstall --user 0 com.facebook.system #Facebook App Installer
+adb shell pm uninstall --user 0 com.google.android.apps.docs #Google Drive
+adb shell pm uninstall --user 0 com.google.android.apps.maps #Google Maps 
+adb shell pm uninstall --user 0 com.google.android.apps.photos #Google Photos
+adb shell pm uninstall --user 0 com.google.android.apps.tachyon #Google Duo
+adb shell pm uninstall --user 0 com.google.android.googlequicksearchbox #Google App
 adb shell pm uninstall --user 0 com.google.android.marvin.talkback
+adb shell pm uninstall --user 0 com.google.android.music #Google Music
 adb shell pm uninstall --user 0 com.google.android.talk
 adb shell pm uninstall --user 0 com.google.android.tts
-adb shell pm uninstall --user 0 com.google.android.youtube
-adb shell pm uninstall --user 0 com.mi.android.globalFileexplorer
+adb shell pm uninstall --user 0 com.google.android.videos #Play Movies
+adb shell pm uninstall --user 0 com.google.android.youtube #Youtube
+adb shell pm uninstall --user 0 com.mfashiongallery.emag
+adb shell pm uninstall --user 0 com.mi.android.globalFileexplorer #Administrador
 adb shell pm uninstall --user 0 com.mi.android.globalpersonalassistant
 adb shell pm uninstall --user 0 com.milink.service
 adb shell pm uninstall --user 0 com.mi.liveassistant
 adb shell pm uninstall --user 0 com.mipay.wallet
+adb shell pm uninstall --user 0 com.miui.analytics #Analytics
 adb shell pm uninstall --user 0 com.miui.android.fashiongallery
 adb shell pm uninstall --user 0 com.miui.antispam
-adb shell pm uninstall --user 0 com.miui.calculator
+adb shell pm uninstall --user 0 com.miui.bugreport #Mi Feedback
+adb shell pm uninstall --user 0 com.miui.calculator #Calculator
 adb shell pm uninstall --user 0 com.miui.cleanmaster
+adb shell pm uninstall --user 0 com.miui.cloudbackup
+adb shell pm uninstall --user 0 com.miui.cloudservice
 adb shell pm uninstall --user 0 com.miui.cloudservice.sysbase
+adb shell pm uninstall --user 0 com.miui.compass #Mi Compass
 adb shell pm uninstall --user 0 com.miui.contentcatcher
 adb shell pm uninstall --user 0 com.miui.daemon
 adb shell pm uninstall --user 0 com.miui.fm
+adb shell pm uninstall --user 0 com.miui.gallery
 adb shell pm uninstall --user 0 com.miui.home
+adb shell pm uninstall --user 0 com.miui.hybrid
 adb shell pm uninstall --user 0 com.miui.klo.bugreport
-adb shell pm uninstall --user 0 com.miui.msa.global
+adb shell pm uninstall --user 0 com.miui.msa.global #Main System Advertising
+adb shell pm uninstall --user 0 com.miui.notes #Mi Notes
 adb shell pm uninstall --user 0 com.miui.personalassistant
+adb shell pm uninstall --user 0 com.miui.player #Mi Music
 adb shell pm uninstall --user 0 com.miui.providers.weather
+adb shell pm uninstall --user 0 com.miui.screenrecorder #Mi Screen Recorder
 adb shell pm uninstall --user 0 com.miui.systemAdSolution
 adb shell pm uninstall --user 0 com.miui.translation.kingsoft
 adb shell pm uninstall --user 0 com.miui.translation.youdao
 adb shell pm uninstall --user 0 com.miui.video
+adb shell pm uninstall --user 0 com.miui.videoplayer #Mi Video
 adb shell pm uninstall --user 0 com.miui.videoplayer.overlay
 adb shell pm uninstall --user 0 com.miui.virtualsim
 adb shell pm uninstall --user 0 com.miui.vsimcore
@@ -52,7 +73,12 @@ adb shell pm uninstall --user 0 com.xiaomi.gamecenter.sdk.service
 adb shell pm uninstall --user 0 com.xiaomi.joyose
 adb shell pm uninstall --user 0 com.xiaomi.jr
 adb shell pm uninstall --user 0 com.xiaomi.lens
+adb shell pm uninstall --user 0 com.xiaomi.midrop #Mi Drop
 adb shell pm uninstall --user 0 com.xiaomi.midrop.overlay
+adb shell pm uninstall --user 0 com.xiaomi.mipicks #Mi Apps
+adb shell pm uninstall --user 0 com.xiaomi.o2o
 adb shell pm uninstall --user 0 com.xiaomi.pass
+adb shell pm uninstall --user 0 com.xiaomi.payment
+adb shell pm uninstall --user 0 com.xiaomi.scanner #Mi Scannercom.miui.screenrecorder
 adb shell pm uninstall --user 0 com.xiaomi.shop
 adb shell pm uninstall --user 0 com.xiaomi.vipaccount
