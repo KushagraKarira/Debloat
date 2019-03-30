290684653061007f61e89c14ae85e9be0994a1b1
Kushagra Karira <kushagra@Adwaita>
2019-03-30 07:11:45 +0530

    Samsung debloat not tested

--- google.sh
+++ google.sh
@@ -6,7 +6,14 @@ adb shell pm uninstall --user 0 com.google.android.apps.tachyon #Google Duo
 adb shell pm uninstall --user 0 com.google.android.googlequicksearchbox #Google App
 adb shell pm uninstall --user 0 com.google.android.marvin.talkback
 adb shell pm uninstall --user 0 com.google.android.music #Google Music
-adb shell pm uninstall --user 0 com.google.android.talk
+adb shell pm uninstall --user 0 com.google.android.talk #Hangouts
 adb shell pm uninstall --user 0 com.google.android.tts
 adb shell pm uninstall --user 0 com.google.android.videos #Play Movies
 adb shell pm uninstall --user 0 com.google.android.youtube #Youtube
+adb shell pm uninstall --user 0 com.google.android.gm #Gmail
+adb shell pm uninstall --user 0 com.android.chrome #GoogleChrome
+adb shell pm uninstall --user 0 com.google.vr.vrcore
+adb shell pm uninstall --user 0 com.google.android.youtube #YouTube
+adb shell pm uninstall --user 0 com.google.android.apps.books
+adb shell pm uninstall --user 0 com.google.android.apps.magazines
+adb shell pm uninstall --user 0 com.google.android.apps.plus #Google+
