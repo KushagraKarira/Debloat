b79bfb60a8bffa7f940b4ab8f1180ed84e6c0da4
Kushagra Karira <kushagra@Adwaita>
2019-03-30 06:51:52 +0530

    Files renamed

---
+++ google.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+adb shell pm uninstall --user 0 com.google.android.apps.docs #Google Drive
+adb shell pm uninstall --user 0 com.google.android.apps.maps #Google Maps 
+adb shell pm uninstall --user 0 com.google.android.apps.photos #Google Photos
+adb shell pm uninstall --user 0 com.google.android.apps.tachyon #Google Duo
+adb shell pm uninstall --user 0 com.google.android.googlequicksearchbox #Google App
+adb shell pm uninstall --user 0 com.google.android.marvin.talkback
+adb shell pm uninstall --user 0 com.google.android.music #Google Music
+adb shell pm uninstall --user 0 com.google.android.talk
+adb shell pm uninstall --user 0 com.google.android.tts
+adb shell pm uninstall --user 0 com.google.android.videos #Play Movies
+adb shell pm uninstall --user 0 com.google.android.youtube #Youtube
