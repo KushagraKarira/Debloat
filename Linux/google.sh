2bae2a554837c7912d59e9d3c0441866c0799d32
kushagrakarira <kushagrakarira@gmail.com>
2019-11-28 17:55:41 +0530

    Update

---
+++ Linux/google.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+adb shell pm uninstall --user 0 com.google.android.apps.docs #Google Drive
+adb shell pm uninstall --user 0 com.google.android.apps.maps #Google Maps 
+adb shell pm uninstall --user 0 com.google.android.apps.photos #Google Photos
+adb shell pm uninstall --user 0 com.google.android.apps.tachyon #Google Duo
+adb shell pm uninstall --user 0 com.google.android.googlequicksearchbox #Google App
+adb shell pm uninstall --user 0 com.google.android.marvin.talkback
+adb shell pm uninstall --user 0 com.google.android.music #Google Music
+adb shell pm uninstall --user 0 com.google.android.talk #Hangouts
+adb shell pm uninstall --user 0 com.google.android.tts
+adb shell pm uninstall --user 0 com.google.android.videos #Play Movies
+adb shell pm uninstall --user 0 com.google.android.youtube #Youtube
+adb shell pm uninstall --user 0 com.google.android.gm #Gmail
+adb shell pm uninstall --user 0 com.android.chrome #GoogleChrome
+adb shell pm uninstall --user 0 com.google.vr.vrcore
+adb shell pm uninstall --user 0 com.google.android.youtube #YouTube
+adb shell pm uninstall --user 0 com.google.android.apps.books
+adb shell pm uninstall --user 0 com.google.android.apps.magazines
+adb shell pm uninstall --user 0 com.google.android.apps.plus #Google+
