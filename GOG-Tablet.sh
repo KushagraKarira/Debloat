3208aa70bd14b7404edc03ab75d3eabd94adc6fe
kushagrakarira <kushagrakarira@gmail.com>
2020-03-10 08:13:26 +0530

    Updated

--- GOG-Tablet.sh
+++ GOG-Tablet.sh
@@ -2,7 +2,6 @@
 adb shell pm uninstall --user 0 com.chaozhuo.filemanager
 adb shell pm uninstall --user 0 com.google.android.apps.tachyon
 adb shell pm uninstall --user 0 com.lenovo.tab7
-adb shell pm uninstall --user 0 com.dolby.dax2appui
 adb shell pm uninstall --user 0 com.dolby.dax2appUI
 adb shell pm uninstall --user 0 com.google.android.videos
 adb shell pm uninstall --user 0 com.google.android.music
